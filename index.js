b3d37e100fda8a6332f081eb981ec0c0ff98afcd
Davin <davin.hayden@outlook.com>
2021-03-03 23:54:32 +0000

    tidied code & comments

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt')
 
 const app = express();
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+//change this URL if using cloud DB like Atlas
 mongoose.connect('mongodb://root:password@localhost:27017', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -19,78 +19,26 @@ mongoose.connect('mongodb://root:password@localhost:27017', {
 })
 
 mongoose.connection.once('open', () => {
-    console.log('connected to database tildb');
+    console.log('Connected to database tildb');
 })
 
 const jwtAuth = expressjwt( {
+    //this turns key into token, must explicity declare which hashing algorithm your using
     secret: 'mossypiglets-and-pangolins',
-    //this turns key into token, must explicity declare which algorithm ur using, its like a hashing algorithm
     credentialsRequired: false,
     algorithms: ['HS256']
 });
 
 app.use(jwtAuth);
 
-app.use(cors({origin:  "http://localhost:3000"}));
-
-
-
-//login resolver stuff
-// login (_, { email, password }) {
-//     const user = await User.findOne({ where: { email } })
-//
-//     if (!user) {
-//         throw new Error('No user with that email')
-//     }
-//
-//     const valid = await bcrypt.compare(password, user.password)
-//
-//     if (!valid) {
-//         throw new Error('Incorrect password')
-//     }
-//
-//     // return json web token
-//     return jsonwebtoken.sign({
-//         id: user.id,
-//         email: user.email
-//     }, 'somesuperdupersecret', { expiresIn: '1y' })
-// }
-
-
-
-
-// login (_, { email, password })
-// {
-//     //does thsi need async await?
-//     const user = UserModel.findOne({ where: { email } })
-//
-//     if (!user) {
-//         throw new Error('No user with that email')
-//     }
-//
-//     //await bcrypt??
-//     const valid = bcrypt.compare(password, user.password)
-//
-//     if (!valid) {
-//         throw new Error('Incorrect password')
-//     }
-//
-//     // return json web token
-//     return jsonwebtoken.sign({
-//         id: user.id,
-//         email: user.email
-//     }, 'mossypiglets-and-pangolins', { expiresIn: '3 hours' })
-// }
-
-
+app.use(cors({origin: "http://localhost:3000"}));
 
 app.use('/graphql', graphqlHTTP({
     schema: require('./schema.js'),
-
     graphiql: true
 }))
 
-//only user with jwt shoudl be able to access this page
+//only users with jwt should be able to access this page
 app.use('/user', bodyParser.json(), jwtAuth, graphqlHTTP(req => ({
         schema: require('./schema.js'),
         context: {
